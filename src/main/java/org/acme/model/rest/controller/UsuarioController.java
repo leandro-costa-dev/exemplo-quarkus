a642d66b6f341d24a1a40733c4cb0f3f4e916235
Leandro Costa <costa_leandro@msn.com>
2023-07-18 20:55:16 -0300

    configuracao keycloack

--- src/main/java/org/acme/model/rest/controller/UsuarioController.java
+++ src/main/java/org/acme/model/rest/controller/UsuarioController.java
@@ -5,11 +5,14 @@ import org.acme.model.rest.dto.UsuarioDTO;
 import org.acme.model.rest.mapper.UsuarioMapper;
 import org.acme.model.service.UsuarioServiceImpl;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.*;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -21,6 +24,15 @@ import java.util.List;
 @Path("/usuarios")// Define o caminho da API rest
 @Consumes(MediaType.APPLICATION_JSON)// informamos que será consumido o JSON na aplicação.
 @Produces(MediaType.APPLICATION_JSON)// a API retornará JSON nas respostas.
+@Tag(name = "Usuario Controller", description = "API de Usuarios")//Swagger
+@SecuritySchemes({ //Autenticar com Token no Swagger
+        @SecurityScheme(
+                securitySchemeName = "usuario-controller-oauth",
+                type = SecuritySchemeType.OAUTH2,
+                flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/realms/realm-exemplo/protocol/openid-connect/token"))
+        )
+})
+@SecurityRequirement(name = "usuario-controller-oauth")// configura o envio do token nas requisições
 public class UsuarioController {
 
     @Inject
@@ -83,9 +95,6 @@ public class UsuarioController {
     @Path("{id}")
     public Response atualizarUsuario(@PathParam("id") Long id, @Valid UsuarioDTO dto) {
         Usuario usuario = service.atualizarUsuario(id, dto);
-
-
-
         return Response.ok(mapper.ConverterUsuarioDTO(usuario)).build();
     }
 }
